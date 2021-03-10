693d1ed94dee531735a3fc47a385f62a3422b532
Shahriyar Shawon <ShahriyarShawon321@gmail.com>
2021-03-09 22:18:09 -0500

    made new directories and render now renders guides

---
+++ render.py
@@ -0,0 +1,30 @@
+from jinja2 import Environment, PackageLoader, select_autoescape
+env = Environment(
+    loader=PackageLoader('shayxyz', 'templates'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+root_dir_files = [
+    "index.html",
+    "musicILike.html",
+    "stuffIUse.html"
+]
+
+for i in root_dir_files:
+    template = env.get_template(i)
+    content = template.render()
+    f = open(i, "w") 
+    f.writelines(content)
+    f.close()
+
+guides = [
+    "index.html",
+    "arch_installation.html"
+]
+
+for i in guides:
+    template = env.get_template("guides/"+i)
+    content = template.render()
+    f = open("guides/"+i, "w") 
+    f.writelines(content)
+    f.close()
