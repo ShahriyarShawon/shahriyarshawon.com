edffc5914c308eb6e6ad7496d9a305c7f5cc4665
Shahriyar Shawon <ShahriyarShawon321@gmail.com>
2021-06-10 18:41:24 -0400

    Attemped to use flask freeze

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/<page>")
+def rootPage(page):
+    return render_template(f"{page}.html")
+
+@app.route("/guides/")
+def guides():
+    return render_template("guides/index.html")
+
+@app.route("/guides/<guide_name>")
+def specificGuide(guide_name):
+    return render_template(f"guides/{guide_name}")
