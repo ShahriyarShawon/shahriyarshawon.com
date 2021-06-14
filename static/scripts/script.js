6d855b77669e732b2919fa260461b717c78302ae
Shahriyar Shawon <ShahriyarShawon321@gmail.com>
2021-06-14 00:02:08 -0400

    Modified Donate Page

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,3 +2,17 @@ function navtoggle(){
     document.getElementById("nav-items").classList.toggle("active");
     console.log("Happened");
 }
+
+function copyXMR() {
+    var copyText = document.getElementById("xmraddr");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
